8b2472e39a3a6386ff4b6b507b2bc57df94ce3bc
OctiAI <project.thirdiris@gmail.com>
2025-06-21 19:53:53 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', 
-            './thankyou.html', 
+            './thanks.html', 
             './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
