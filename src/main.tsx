4b6d373c073a0f41db1b35716e0297481f43206d
OctiAI <project.thirdiris@gmail.com>
2025-06-23 21:33:46 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import AnalyticsStuff from './components/AnalyticsStuff';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AnalyticsStuff />
     <App />
   </StrictMode>
 );
